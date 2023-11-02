87f30328c6df9a6cc2356c08e36e3897f8ff006b
Sabrina Beukes <sabrinabeukes@gmail.com>
2023-11-02 07:48:55 -0700

    fixes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     elif nr == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(count))
-    for i in range(count):
+        print("{} arguments:".format(nr))
+    for i in range(nr):
         print("{}: {}".format(i + 1, argv[i + 1]))
