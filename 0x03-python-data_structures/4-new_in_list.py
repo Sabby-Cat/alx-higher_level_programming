ad7cdef255f1b9f07ab91749e9ac2dc535238068
Sabrina Beukes <sabrinabeukes@gmail.com>
2023-11-03 02:18:35 -0700

    fixes

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    
+    dup = my_list.copy()
+    if idx >= 0 and idx < len(my_list):
+        dup[idx] = element
+        return dup
+    else:
+        return dup
