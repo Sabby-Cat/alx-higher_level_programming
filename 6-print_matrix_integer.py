1cc12129297ddfc02770c5d1c39b026775195583
Sabby-Cat <122835289+Sabby-Cat@users.noreply.github.com>
2023-11-05 19:54:26 +0200

    q6 to q12

--- 6-print_matrix_integer.py
+++ 6-print_matrix_integer.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    l
+    for c in matrix:
+        for r in c:
+            print("{:d}".format(c), end=" " if c != r[-1] else ""))
+        print()
