9f70b73661bda522a11a9657db9057f8b0d79f39
Sabrina Beukes <sabrinabeukes@gmail.com>
2023-11-06 04:14:06 -0800

    fixes

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    dict = {'c': None, 'C': None}
+    dict = {67: 0, 99: 0}
     return my_string.translate(dict)
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for c in matrix:
         for r in c:
-            print("{:d}".format(c), end=" " if c != r[-1] else ""))
-        print()
+            print("{:d}".format(r), end=" " if c != r[-1] else "")
+            print()
