74465bd07078aa6d4bbc1fb7439da99591313ccb
Sabrina Beukes <sabrinabeukes@gmail.com>
2023-11-06 06:22:56 -0800

    fixes

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -2,19 +2,19 @@
 #include <object.h>
 #include <listobject.h>
 /**
- *
- *
+ * print_python_list_info - prints info
+ * @p: pyobject
  */
- void print_python_list_info(PyObject *p)
- {
-   long int size = PyList_Size(p);
+void print_python_list_info(PyObject *p)
+{
+	long int size = PyList_Size(p);
 	PyListObject *obj = (PyListObject *)p;
-  int i;
+	int i;
 
 	printf("[*] Size of the Python List = %li\n", size);
 	printf("[*] Allocated = %li\n", obj->allocated);
 	for (i = 0; i < size; i++)
-  {
+	{
 		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
-  }
- }
+	}
+}
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
     dict = {67: 0, 99: 0}
-    return my_string.translate(dict)
+    new = my_string.translate(dict)
+    return new
