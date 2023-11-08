a3f11d94d2d2f86ed30d5ff0a6a3d9f77ccb66ca
Sabby-Cat <122835289+Sabby-Cat@users.noreply.github.com>
2023-11-08 11:36:31 +0200

    fixes

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     new = matix.copy()
     for r in new:
-        new[r] = list(map(lambda x: x** 2, new[r]))
+        new[r] = list(map(lambda x: x ** 2, new[r]))
     return new
