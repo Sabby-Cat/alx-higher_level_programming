6075a376bc3562b1805745df9b7dfadcbada7ad6
Sabby-Cat <122835289+Sabby-Cat@users.noreply.github.com>
2023-11-27 12:19:43 +0200

    fixes

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -20,6 +20,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -32,6 +33,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
         """gets area of rec"""
