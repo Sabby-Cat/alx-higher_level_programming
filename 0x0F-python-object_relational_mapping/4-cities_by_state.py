ba74d2cb8ead496f1c0e4ff172f582ff60fcdeec
Sabby-Cat <122835289+Sabby-Cat@users.noreply.github.com>
2024-02-17 17:54:48 +0200

    fix

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    sto = db.cusor()
+    sto = db.cursor()
     sto.execute("""SELECT cities.id, cities.name, states.name FROM
                 cities INNER JOIN states ON states.id=cities.state_id""")
     lines = sto.fetchall()
