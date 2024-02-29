3aa49add850f942d6245d91c113f73956fc3c8ae
Sabby-Cat <122835289+Sabby-Cat@users.noreply.github.com>
2024-02-29 22:02:00 +0200

    fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,10 +6,10 @@ def find_peak(list_of_integers):
     """finds num that's greater than both left and right"""
     if not list_of_integers:
         return None
-    start = 0
+    begin = 0
     end = len(list_of_integers)-1
-    if list_of_integers[start] > list_of_integers[start+1]:
-        return list_of_integers[start]
+    if list_of_integers[begin] > list_of_integers[begin+1]:
+        return list_of_integers[begin]
     if list_of_integers[end] > list_of_integers[end-1]:
         return list_of_integers[end]
     mid = len(list_of_integers) // 2
@@ -18,8 +18,8 @@ def find_peak(list_of_integers):
             and list_of_integers[mid + 1] < mid_val:
         return mid_val
     if mid_val < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[start:mid + 1])
-     elif mid_val < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[begin:mid + 1])
+    elif mid_val < list_of_integers[mid + 1]:
         return find_peak(list_of_integers[mid:end + 1])
     else:
-        return list_of_integers[start]
+        return list_of_integers[begin]
